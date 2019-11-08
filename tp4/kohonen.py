deeceadfdb509a7bccf5cd8be71e7f78952a9f19
marcelolynch <mlynch@itba.edu.ar>
2019-11-08 18:40:19 -0300

    Comentarios

--- tp4/kohonen.py
+++ tp4/kohonen.py
@@ -16,30 +16,32 @@ def normalize(x):
     return x / np.linalg.norm(x)
 
 class Kohonen:
-    def fit(self, x, dim, epochs = 1000, alpha_initial = 0.1, alpha_decay = None, neighborhood = None):
-        X = np.array([normalize(x_i) for x_i in x])
+    def fit(self, X, dim, epochs = 1000, alpha = None, neighborhood = None):
+        X = np.array([normalize(x_i) for x_i in X])     # Normalizar los ejemplos
 
         if neighborhood is None:
             neighborhood = neighborhood_linear(dim/2, epochs/2)
         
-        if alpha_decay is None:
-            alpha_decay = linear_decay(alpha_initial, epochs)
+        if alpha is None:
+            alpha = linear_decay(0.1, epochs)
+
+        self.network = []   # Cada nodo es un dict {x, y, weights}
+        c = 0
 
         np.random.shuffle(X)
-        self.network = [] # Lista de {x, y, weights}
-        c = 0
         for i in range(0, dim):
             for j in range(0, dim):
-                self.network.append({"x": i, "y": j, "weights": np.array(X[c])})    # Inicializo con pesos aleatorios
+                # Inicializo con pesos tomando elementos aleatorios del conjunto de entrenamiento
+                self.network.append({"x": i, "y": j, "weights": np.array(X[c])})    
                 c += 1
 
         for t in range(1, epochs):
-            np.random.shuffle(X)
+            np.random.shuffle(X)    # Voy a recorrerlos todos en un orden aleatorio
             for x_i in X:
                 distances = [np.dot(x_i, n["weights"]) for n in self.network ]
                 winner = self.network[np.argmin(distances)]
                 for n in self.network:
-                    n["weights"] = n["weights"] + neighborhood(t, grid_distance(n, winner)) * alpha_decay(t) * (x_i - n["weights"]) 
+                    n["weights"] = n["weights"] + neighborhood(t, grid_distance(n, winner)) * alpha(t) * (x_i - n["weights"]) 
     
     def predict(self, x):
         x = normalize(x)
